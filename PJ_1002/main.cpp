df00e8015a71db7eac0fa504e434cba24acc189f
Lumy0726 <jinny9807@gmail.com>
2017-03-27 13:21:38 +0900

    PJ_1002 complate

--- PJ_1002/main.cpp
+++ PJ_1002/main.cpp
@@ -52,15 +52,20 @@ int main(int arg_num, char * argv[], char * env[]) {
 
 //function define
 int Target_num(int x1, int y1, unsigned r1, int x2, int y2, unsigned r2) {
-	unsigned Distance;
+	unsigned Distance, Add, Sub;
 	if (x1 == x2 && y1 == y2) {//두 좌표가 같은 경우.
 		if (r1 == r2) { return -1; }
 		else { return 0; }
 	}
 	else {//두 좌표가 다른 경우.
-		"Code add";
-		"";
-		return 0;
+		Distance = unsigned((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));//두 좌표의 거리의 제곱.
+		Add = unsigned((r1 + r2)*(r1 + r2));//두 반지름 합의 제곱.
+		Sub = unsigned((r1 - r2)*(r1 - r2));//두 반지름 차의 제곱.
+		if (Add < Distance) { return 0; }
+		else if (Add == Distance) { return 1; }
+		else if (Sub < Distance) { return 2; }
+		else if (Sub == Distance) { return 1; }
+		else { return 0; }
 	}
 }
 //verge
